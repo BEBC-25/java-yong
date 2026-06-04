a1fcb7274da2d26eb9567f02ab76aab47a439f7b
kilyong jeong <uzoolove@gmail.com>
2026-06-04 11:51:27 +0900

    생성자 추가, getter/setter 추가

--- workspace/ch11/capsule/ex01/ScoreTest.java
+++ workspace/ch11/capsule/ex01/ScoreTest.java
@@ -1,4 +1,4 @@
-package ch11.capsule;
+package ch11.capsule.ex01;
 
 public class ScoreTest {
     public static void main(String[] args){
@@ -6,9 +6,11 @@ public class ScoreTest {
         haru.kor = 100;
         haru.eng = 90;
         haru.math = 85;
-        int haruSum = haru.sum();
-        double haruAvg = haruSum / 3;
-        System.out.println("하루의 총점: " + haruSum);
+
+//        int haruSum = haru.sum();
+//        System.out.println("하루의 총점: " + haruSum);
+
+        double haruAvg = haru.avg();
         System.out.println("하루의 평균: " + haruAvg);
     }
 }
