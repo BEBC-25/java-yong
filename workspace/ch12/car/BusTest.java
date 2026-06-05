1e2437b4ac11a69a425f49bb9b8f2fec2846e256
kilyong jeong <uzoolove@gmail.com>
2026-06-05 15:35:56 +0900

    bus 실습 작성중...

--- workspace/ch12/car/BusTest.java
+++ workspace/ch12/car/BusTest.java
@@ -2,24 +2,19 @@ package ch12.car;
 
 public class BusTest {
     void main(){
-        Car mycar = new Bus("람보르기니");
-        mycar.speed = 100;
-
+        Bus mycar = new Bus("람보르기니", "10-2", "마을 버스"
+                        , new String[]{"우리집", "김포고등학교", "김포 시청"}
+                        , 1300, 15);
         mycar.start();
         mycar.speedUp();
         mycar.speedUp();
         mycar.speedUp();
-        mycar.gearUp();
         mycar.speedUp();
         mycar.speedUp();
-        mycar.gearUp();
-        mycar.speedUp();
-        mycar.speedUp();
-        mycar.speedUp();
-        mycar.gearUp();
-        mycar.speedUp();
-        mycar.speedUp();
-
         mycar.stop();
+
+        mycar.start();
+
+        System.out.println(mycar.getBusInfo());
     }
 }
