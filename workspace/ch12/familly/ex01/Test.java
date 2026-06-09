487aca2b3f74e5c8659b7c5a08a3fb84cb9780da
kilyong jeong <uzoolove@gmail.com>
2026-06-09 09:00:07 +0900

    패키지명 변경

--- workspace/ch12/familly/ex01/Test.java
+++ workspace/ch12/familly/ex01/Test.java
@@ -1,4 +1,4 @@
-package ch12.family.ex01;
+package ch12.familly.ex01;
 
 public class Test {
 
@@ -18,14 +18,14 @@ public class Test {
     }
 
     // 하루 일과
-    private static void dailySchedule(Son p){ // Parent 타입의 매개 변수 정의
+    private static void dailySchedule(Son p){ // Son 타입의 매개 변수 정의
         p.eat();
         p.play();
         p.sleep();
     }
 
     // 하루 일과
-    private static void dailySchedule(Daughter p){ // Parent 타입의 매개 변수 정의
+    private static void dailySchedule(Daughter p){ // Daughter 타입의 매개 변수 정의
         p.eat();
         p.play();
         p.sleep();
