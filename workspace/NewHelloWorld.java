5e687a6c997510216eca8d78fb342507f8e2c8cf
kilyong jeong <uzoolove@gmail.com>
2026-06-12 11:36:35 +0900

    out 폴더 제외

---
+++ workspace/NewHelloWorld.java
@@ -0,0 +1,10 @@
+public class NewHelloWorld {
+    public static void main(String[] args){
+        System.out.println("New Hello World!!!");
+    }
+
+//    void main(){
+//        System.out.println("New Hello World!!!");
+//    }
+}
+
