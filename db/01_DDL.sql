478b57267ef6c023e8768527dfbcdd3931697ea5
kilyong jeong <uzoolove@gmail.com>
2026-06-12 11:23:36 +0900

    폴더 정리

--- db/01_DDL.sql
+++ db/01_DDL.sql
@@ -22,6 +22,33 @@ create table member (
 );
 
 -- 게시글 테이블 생성
+create table post(
+                     id int auto_increment primary key,
+                     user_id int not null ,
+                     title varchar(100)	not null,
+                     content text not null,
+                     created_at datetime default current_timestamp,
+                     foreign KEY (user_id) references member(id) on delete CASCADE
+);
+
+
+insert into post (user_id, title, content) values
+                                               (1,'안녕하세요.', '반가워요' ),
+                                               (2,'질문 있어요.', 'DB는 어렵나요?'),
+                                               (1,'두번째 게시글', '오늘 날씨가 좋네요'),
+                                               (2,'MySQL 설치 오류가 났어요', '어떻게 할까요?'),
+                                               (3,'자기소개', '안녕하세요. 용쌤입니다.')
+;
+
 
 -- 댓글 테이블 생성
 
+create table reply (
+                       id int auto_increment primary key,
+                       post_id int not null,
+                       user_id int not null,
+                       content text not null,
+                       created_at datetime default current_timestamp,
+                       foreign key (post_id) references post(id) on delete cascade,
+                       foreign key (user_id) references member(id) on delete cascade
+);
