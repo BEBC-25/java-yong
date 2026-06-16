2ae1a15700d5dfee4a98f1ac602b84f2346b07ae
kilyong jeong <uzoolove@gmail.com>
2026-06-16 13:51:14 +0900

    join 실습중

--- workspace/03_database/query/02_DML/select-join.sql
+++ workspace/03_database/query/02_DML/select-join.sql
@@ -42,6 +42,14 @@ SELECT p.id, p.member_id, p.title, p.view_count, m.name, m.email
 FROM post p
 LEFT JOIN member AS m ON p.member_id = m.id;
 
+-- 전체 회원 목록과 각 회원이 작성한 게시글 수를 조회하세요.(게시글을 작성하지 않은 회원도 포함되어야 함)
+-- 전체 회원 목록 조회
+SELECT * FROM member;
+-- 필요한 컬럼만 추출
+SELECT m.id, m.name, COUNT(p.id) post_count
+FROM member m
+LEFT JOIN post p ON m.id = p.member_id
+GROUP BY m.id, m.name;
 
 -- RIGHT OUTER JOIN
 -- 모든 게시글의 id, member_id, title, view_count, 작성자이름, 작성자이메일 컬럼 조회(alias 사용)
@@ -54,6 +62,32 @@ RIGHT JOIN member AS m ON p.member_id = m.id;
 SELECT p.id, p.member_id, p.title, p.view_count, m.name, m.email
 FROM member m
 LEFT JOIN post AS p ON p.member_id = m.id;
+
+
+-- FULL OUTER JOIN(MySQL은 지원하지 않으므로 UNION으로 대체 가능)
+SELECT m.id AS member_id, m.name, p.title
+FROM member m
+LEFT JOIN post p ON m.id = p.member_id
+UNION
+SELECT m.id AS member_id, m.name, p.title
+FROM member m
+RIGHT JOIN post p ON m.id = p.member_id;
+
+
+
+-- SELF JOIN
+-- 회원명과 추천인명을 조회
+SELECT m1.name 회원명, m1.recommender_id, IFNULL(m2.name, '-') 추천인명
+FROM member m1
+LEFT JOIN member m2 ON m1.recommender_id = m2.id; -- 모든 회원명과 추천인명을 조회
+-- INNER JOIN member m2 ON m1.recommender_id = m2.id; -- 추천인이 있는 회원명과 추천인명을 조회
+
+
+
+
+
+
+
 
 
 
