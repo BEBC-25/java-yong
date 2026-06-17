f72e03b120b643247a81c8b89eb651fa1423154c
kilyong jeong <uzoolove@gmail.com>
2026-06-17 16:21:27 +0900

    회원 가입 추가

--- workspace/03_database/ch07/JdbcBasicTest.java
+++ workspace/03_database/ch07/JdbcBasicTest.java
@@ -12,7 +12,9 @@ public class JdbcBasicTest {
     private static final String DB_PASSWORD = "1111";
 
     public static void main(String[] args){
-        selectAllMembers();
+        selectAllMembers(); // 회원 목록 조회
+        insertMember("haru" + (int)(Math.random() * 1000) + "@gmail.com", "1234", "뉴하루", "01022221111", 2); // 회원 등록
+        selectAllMembers(); // 회원 목록 조회
     }
 
     // 회원 목록 조회
@@ -53,7 +55,46 @@ public class JdbcBasicTest {
     }
 
     // 회원 등록
-    public static void insertMember(){
+    public static void insertMember(String email, String password, String name, String phone, int recommenderId){
+        Connection conn = null;
+        Statement stmt = null;
 
+        try{ // 플랜 A
+            // 1. 데이터베이스 연결(Connection 객체 생성)
+            conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+
+            // 2. SQL 실행 객체 생성(Statement 객체 생성)
+            stmt = conn.createStatement();
+
+            // 3. SQL 실행(SELECT)
+            // 4. 결과 수신(ResultSet 객체 생성)
+            int affectedRows = stmt.executeUpdate("INSERT INTO member (email, password, name, phone, recommender_id) VALUES \n" +
+                    "    ('"+email+"', '"+password+"', '"+name+"', '"+phone+"', "+recommenderId+")");
+
+            System.out.println("회원 등록 완료: " + affectedRows + "건 반영됨.");
+
+        }catch(Exception e){ // 플랜 B
+            System.out.println("에러 발생: " + e.getMessage());
+            e.printStackTrace();
+        }finally{
+            // 5. 생성된 리소스를 생성의 역순으로 해제
+            try{ if(stmt != null) stmt.close(); } catch (Exception e){ }
+            try{ if(conn != null) conn.close(); } catch (Exception e){ }
+        }
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
