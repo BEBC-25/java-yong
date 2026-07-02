9ce984510cd0ca7f215947380edd929cee800638
kilyong jeong <uzoolove@gmail.com>
2026-07-02 17:36:15 +0900

    .

--- workspace/04_advanced/ch04/echo/EchoClient.java
+++ workspace/04_advanced/ch04/echo/EchoClient.java
@@ -7,8 +7,8 @@ public class EchoClient {
     void startClient(){
         try(
                 // 서버에 접속 요청(localhost:50000)
-//                Socket s = new Socket("localhost", 50000);
-                Socket s = new Socket("dain2.iptime.org", 50000);
+                Socket s = new Socket("localhost", 50000);
+//                Socket s = new Socket("dain2.iptime.org", 50000);
                 // Socket에서 데이터를 읽을 수 있는 InputStream 생성
                 InputStream in = s.getInputStream();
                 // Socket으로 데이터를 전송할 수 있는 OutputStream 생성
---
+++ workspace/programmers/level02/day04/Prob04.java
@@ -0,0 +1,33 @@
+/* 배열의 평균값
+ *
+ * [문제 설명]
+ * 정수 배열 numbers가 매개변수로 주어질 때, numbers의 원소들의 평균값을 return 하도록 solution 함수를 완성해주세요.
+ *
+ * [제한사항]
+ * 0 <= numbers의 원소 <= 1000
+ * 1 <= numbers의 길이 <= 100
+ * 정답의 소수 부분이 .0 또는 .5인 경우만 입력으로 주어집니다.
+ *
+ * [입출력 예]
+ * numbers | result
+ * [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] | 5.5
+ * [89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99] | 94.0
+ *
+ * [입출력 예 설명]
+ * 예제 1번: numbers의 원소들의 합은 55이고, numbers의 길이는 10입니다. 55 / 10 = 5.5를 return 합니다.
+ * 예제 2번: numbers의 원소들의 합은 1034이고, numbers의 길이는 11입니다. 1034 / 11 = 94.0을 return 합니다.
+ */
+
+package level02.day04;
+
+public class Prob04 {
+    public double solution(int[] numbers) {
+        double answer = 0;
+        return answer;
+    }
+
+    void main() {
+        System.out.println(solution(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}));
+        System.out.println(solution(new int[]{89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99}));
+    }
+}
