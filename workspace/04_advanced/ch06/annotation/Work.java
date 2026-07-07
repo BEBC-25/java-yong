fe7307bd6996e0af21472c94ec467521c198ac79
kilyong jeong <uzoolove@gmail.com>
2026-07-07 10:08:37 +0900

    Custom Annotation 작성1

--- workspace/04_advanced/ch06/annotation/Work.java
+++ workspace/04_advanced/ch06/annotation/Work.java
@@ -2,7 +2,11 @@ package ch06.annotation;
 
 public class Work {
     @MeasureTime
-    public void task1(){}
+    public void task1(){
+        System.out.println("task1 실행");
+    }
 
-    public void task2(){}
+    public void task2(){
+        System.out.println("task2 실행");
+    }
 }
