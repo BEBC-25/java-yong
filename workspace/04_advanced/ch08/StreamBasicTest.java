47d11ca051ee06e4c778528b461bef4e42735e22
kilyong jeong <uzoolove@gmail.com>
2026-09-08 10:50:32 +0900

    Stream API 실습

---
+++ workspace/04_advanced/ch08/StreamBasicTest.java
@@ -0,0 +1,38 @@
+package ch08;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StreamBasicTest {
+
+    List<String> names = List.of("haru", "namu", "brong", "harong", "haru");
+
+    void forLoop(){
+        List<String> filteredName = new ArrayList();
+        for(int i=0; i<names.size(); i++){
+            String name = names.get(i);
+            if(name.startsWith("h")){ // h로 시작하는 이름만 대문자로 저장
+                filteredName.add(name.toUpperCase());
+            }
+        }
+        System.out.println(filteredName);
+    }
+
+    void streamApi(){
+        List<String> filteredName = names.stream()
+                .filter(name -> name.startsWith("h")) // h로 시작하는 이름만 필터링
+                .map(String::toUpperCase) // 대문자로 변환
+                .sorted()
+//                .distinct()
+                .skip(1)
+                .limit(1)
+                .toList(); // 리스트로 반환
+        System.out.println(filteredName);
+    }
+
+    void main(){
+        forLoop();
+        streamApi();
+    }
+
+}
